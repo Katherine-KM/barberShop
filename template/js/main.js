415d0be9be2b11485d940f1bb16523c409c92242
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2022-06-11 17:15:46 -0400

    change nav bar and make it responsive

--- template/js/main.js
+++ template/js/main.js
@@ -1,3 +1,5 @@
+// portfolio lightbox effect
+
 const lightbox = document.createElement('div');
 lightbox.id = 'lightbox';
 document.body.appendChild(lightbox)
@@ -19,3 +21,18 @@ lightbox.addEventListener('click', e => {
     if (e.target !== e.currentTarget) return
     lightbox.classList.remove('active')
 })
+
+
+// hamburger menu effect
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
